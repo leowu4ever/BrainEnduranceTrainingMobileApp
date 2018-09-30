f1177e2897f04325c37c97cb86e5c8bf59f8b1c5
Leo Wu <leowu4ever@users.noreply.github.com>
2018-09-30 22:10:55 +0100

    Be able to read accelerometer.

--- app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/MainActivity.java
+++ app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/MainActivity.java
@@ -1,21 +1,50 @@
 package com.kent.lw.brainendurancetrainingmobileapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sm;
+    private Sensor s;
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        sm = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+            // use accelerometer
+        if(sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            s = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sm.registerListener(this, s, SensorManager.SENSOR_DELAY_FASTEST);
+            Log.d("acc", "found ");
+
+        } else {
+            Log.d("acc", "not found");
+        }
+
+
+        tv = findViewById(R.id.tv_acc);
+        tv.setText("yes");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        tv.setText("X: " + event.values[0] + "\nY: " + event.values[1] + "\nZ: " + event.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
