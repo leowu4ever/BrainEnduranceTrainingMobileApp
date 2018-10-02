bad4f87d31b33dd359f831404db51fcee359684e
Leo Wu <leowu4ever@users.noreply.github.com>
2018-10-02 10:32:05 +0100

    Flic grabber is ready

--- app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/MainActivity.java
+++ app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/MainActivity.java
@@ -12,6 +12,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import io.flic.lib.FlicAppNotInstalledException;
+import io.flic.lib.FlicManager;
+import io.flic.lib.FlicManagerInitializedCallback;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -46,6 +51,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+
+        FlicManager.setAppCredentials("ddbfde99-d965-41df-8b9d-810bb0c26fe7", "f6e6938e-4d36-46e6-8fe1-d38436bdef83", "Brain Endurance Training Mobile App");
+
+
+        Button btnFlic = (Button) findViewById(R.id.btn_flic);
+        btnFlic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FlicManager.getInstance(MainActivity.this, new FlicManagerInitializedCallback() {
+                        @Override
+                        public void onInitialized(FlicManager manager) {
+                            manager.initiateGrabButton(MainActivity.this);
+                        }
+                    });
+                } catch (FlicAppNotInstalledException err) {
+                    Toast.makeText(MainActivity.this, "Flic App is not installed", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
