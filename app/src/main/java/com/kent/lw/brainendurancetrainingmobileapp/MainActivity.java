3ef7eab6735d1750205cc13ddd587a4e8eda4c89
Leo Wu <leowu4ever@users.noreply.github.com>
2018-10-07 12:22:49 +0100

    Add a graph sdk screen remove unused btns

--- app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/MainActivity.java
+++ app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/MainActivity.java
@@ -14,16 +14,29 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import io.flic.lib.FlicAppNotInstalledException;
 import io.flic.lib.FlicManager;
 import io.flic.lib.FlicManagerInitializedCallback;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, OnMapReadyCallback {
 
     private SensorManager sm;
     private Sensor s;
     private TextView tv;
+
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -44,16 +57,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         tv = findViewById(R.id.tv_acc);
         tv.setText("yes");
-
-        Button btnMap = findViewById(R.id.btn_map);
-        btnMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(intent);
-            }
-        });
-
 
 
         FlicManager.setAppCredentials("ddbfde99-d965-41df-8b9d-810bb0c26fe7", "f6e6938e-4d36-46e6-8fe1-d38436bdef83", "Brain Endurance Training Mobile App");
@@ -104,17 +107,48 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         });
 
+        GraphView graph = findViewById(R.id.graph1);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
-
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        tv.setText("X: " + event.values[0] + "\nY: " + event.values[1] + "\nZ: " + event.values[2]);
+        tv.setText("(X: " + event.values[0] + ")      (Y: " + event.values[1] + ")      (Z: " + event.values[2] + ")");
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng medway = new LatLng(51.3, 0.54);
+        mMap.addMarker(new MarkerOptions().position(medway).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(medway));
     }
 }
