053d87a7e5b964ea2077ae897013007f102f1df1
Leo Wu <leowu4ever@users.noreply.github.com>
2018-10-10 12:48:55 +0100

    Tweak task screen

--- app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/TaskActivity.java
+++ app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/TaskActivity.java
@@ -1,7 +1,10 @@
 package com.kent.lw.brainendurancetrainingmobileapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class TaskActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class TaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TaskActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
