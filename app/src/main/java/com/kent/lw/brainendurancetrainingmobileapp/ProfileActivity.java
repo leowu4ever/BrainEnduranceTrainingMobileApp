cc1311e55c5e063628e6479d58e5a16115907a59
Leo Wu <leowu4ever@users.noreply.github.com>
2018-10-10 12:40:34 +0100

    Structure record screen

--- app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/ProfileActivity.java
+++ app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/ProfileActivity.java
@@ -1,7 +1,10 @@
 package com.kent.lw.brainendurancetrainingmobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        Button btnRecord = findViewById(R.id.btn_record);
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, RecordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnNewTraining = findViewById(R.id.btn_new_training);
+        btnNewTraining.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, TaskActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
