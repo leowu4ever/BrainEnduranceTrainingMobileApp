70bdccd65368974a4bb1ce99f2e559bec9a21ba2
Leo Wu <leowu4ever@users.noreply.github.com>
2018-11-20 20:10:33 +0000

    Ready to work on history dialog

--- app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/OverallPerformance.java
+++ app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/OverallPerformance.java
@@ -1,6 +1,46 @@
 package com.kent.lw.brainendurancetrainingmobileapp;
 
+import java.util.List;
+
 public class OverallPerformance {
+
+    public float overallRT;
+    public float overallAccuracy;
+    public List<Float> rtList;
+    public List<Float> accuracyList;
+
+    public float getOverallRT() {
+        return overallRT;
+    }
+
+    public void setOverallRT(float overallRT) {
+        this.overallRT = overallRT;
+    }
+
+    public float getOverallAccuracy() {
+        return overallAccuracy;
+    }
+
+    public void setOverallAccuracy(float overallAccuracy) {
+        this.overallAccuracy = overallAccuracy;
+    }
+
+    public List<Float> getRtList() {
+        return rtList;
+    }
+
+    public void setRtList(List<Float> rtList) {
+        this.rtList = rtList;
+    }
+
+    public List<Float> getAccuracyList() {
+        return accuracyList;
+    }
+
+    public void setAccuracyList(List<Float> accuracyList) {
+        this.accuracyList = accuracyList;
+    }
+
 
 
 }
