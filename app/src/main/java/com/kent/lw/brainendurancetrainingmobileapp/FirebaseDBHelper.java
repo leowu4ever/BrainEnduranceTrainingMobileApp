9e4fd69335fab3736a02178df4ff7bea36519320
Leo Wu <leowu4ever@users.noreply.github.com>
2018-12-03 12:54:27 +0000

    Redesign db name convention

--- app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/FirebaseDBHelper.java
+++ app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/FirebaseDBHelper.java
@@ -33,13 +33,14 @@ public class FirebaseDBHelper {
         db.child(rootPath + "3_Cognitive Performance" + "/" + "6_Average response time").setValue(MainActivity.trainingData.getAvgResTime());
 
         db.child(rootPath + "4_Physical Performance" + "/" + "1_Distance").setValue(MainActivity.trainingData.getDistance());
-        db.child(rootPath + "4_Physical Performance" + "/" + "2_Speed").setValue(MainActivity.trainingData.getAvgSpeed());
-        db.child(rootPath + "4_Physical Performance" + "/" + "3_Pace").setValue(MainActivity.trainingData.getAvgPace());
-        db.child(rootPath + "4_Physical Performance" + "/" + "4_Speed list").setValue(MainActivity.trainingData.getAvgPace());
+        db.child(rootPath + "4_Physical Performance" + "/" + "2_Average speed").setValue(MainActivity.trainingData.getAvgSpeed());
+        db.child(rootPath + "4_Physical Performance" + "/" + "3_Average pace").setValue(MainActivity.trainingData.getAvgPace());
+        db.child(rootPath + "4_Physical Performance" + "/" + "4_Speed list").setValue(MainActivity.trainingData.getSpeedList());
 
         db.child(rootPath + "5_Stimulus Record" + "/" + "1_Stimulus mili list").setValue(MainActivity.trainingData.getStiMiliList());
         db.child(rootPath + "5_Stimulus Record" + "/" + "2_Responses mili list").setValue(MainActivity.trainingData.getResMiliList());
         db.child(rootPath + "5_Stimulus Record" + "/" + "3_Responses time list").setValue(MainActivity.trainingData.getResTimeList());
+        db.child(rootPath + "5_Stimulus Record" + "/" + "4_Stimulus type list").setValue(MainActivity.trainingData.getStiTypeList());
 
         db.child(rootPath + "6_Location" + "/" + "1_Latitude list").setValue(MainActivity.trainingData.getLatList());
         db.child(rootPath + "6_Location" + "/" + "2_Longitude list").setValue(MainActivity.trainingData.getLngList());
