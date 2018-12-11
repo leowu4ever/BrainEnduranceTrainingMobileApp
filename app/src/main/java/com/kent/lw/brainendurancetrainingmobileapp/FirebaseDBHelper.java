56dc2b52b65cd9981c237232059d435e489b08b1
Leo Wu <leowu4ever@users.noreply.github.com>
2018-12-11 19:30:27 +0000

    Fasten realtime db

--- app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/FirebaseDBHelper.java
+++ app/src/main/java/com/kent/lw/brainendurancetrainingmobileapp/FirebaseDBHelper.java
@@ -41,11 +41,11 @@ public class FirebaseDBHelper {
         db.child(rootPath + "4_Physical Performance" + "/" + "1_Distance").setValue(trainingData.getDistance());
         db.child(rootPath + "4_Physical Performance" + "/" + "2_Average speed").setValue(trainingData.getAvgSpeed());
         db.child(rootPath + "4_Physical Performance" + "/" + "3_Average pace").setValue(trainingData.getAvgPace());
-        db.child(rootPath + "4_Physical Performance" + "/" + "4_Speed list").setValue(trainingData.getSpeedList());
+        //db.child(rootPath + "4_Physical Performance" + "/" + "4_Speed list").setValue(trainingData.getSpeedList());
 
-        db.child(rootPath + "5_Stimulus Record" + "/" + "1_Stimulus mili list").setValue(trainingData.getStiMiliList());
-        db.child(rootPath + "5_Stimulus Record" + "/" + "2_Responses mili list").setValue(trainingData.getResMiliList());
-        db.child(rootPath + "5_Stimulus Record" + "/" + "3_Responses time list").setValue(trainingData.getResTimeList());
+        //db.child(rootPath + "5_Stimulus Record" + "/" + "1_Stimulus mili list").setValue(trainingData.getStiMiliList());
+        //db.child(rootPath + "5_Stimulus Record" + "/" + "2_Responses mili list").setValue(trainingData.getResMiliList());
+        //db.child(rootPath + "5_Stimulus Record" + "/" + "3_Responses time list").setValue(trainingData.getResTimeList());
         db.child(rootPath + "5_Stimulus Record" + "/" + "4_Stimulus type list").setValue(trainingData.getStiTypeList());
 
         db.child(rootPath + "6_Location" + "/" + "1_Latitude list").setValue(trainingData.getLatList());
